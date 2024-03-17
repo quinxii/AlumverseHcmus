2865bb7c551a1ccbf078fd8140541973b0d31272
quinxii <82009267+quinxii@users.noreply.github.com>
2024-03-17 17:00:58 +0700

    Update AuthorizationCodeGeneratorUtils.java
    update

--- user-service/src/main/java/hcmus/alumni/userservice/utils/AuthorizationCodeGeneratorUtils.java
+++ user-service/src/main/java/hcmus/alumni/userservice/utils/AuthorizationCodeGeneratorUtils.java
@@ -3,14 +3,31 @@ package hcmus.alumni.userservice.utils;
 import java.security.SecureRandom;
 
 public class AuthorizationCodeGeneratorUtils {
+	
 	private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    private static final int CODE_LENGTH = 8; // You can adjust the length as needed
+    private static final int CODE_LENGTH = 8; 
 
-    public static String generateAuthorizeCode() {
+    private static volatile AuthorizationCodeGeneratorUtils instance  = null;
+    
+    private AuthorizationCodeGeneratorUtils() {
+        super();
+    }
+	
+	public static AuthorizationCodeGeneratorUtils getInstance() {
+        if (instance == null) {
+            synchronized (AuthorizationCodeGeneratorUtils.class) {
+                if (instance == null) {
+                    instance = new AuthorizationCodeGeneratorUtils();
+                }
+            }
+        }
+        return instance;
+    }
+    
+    public String generateAuthorizeCode() {
         SecureRandom random = new SecureRandom();
         StringBuilder sb = new StringBuilder(CODE_LENGTH);
 
-        // Generate random characters from CHARACTERS string
         for (int i = 0; i < CODE_LENGTH; i++) {
             int randomIndex = random.nextInt(CHARACTERS.length());
             sb.append(CHARACTERS.charAt(randomIndex));
