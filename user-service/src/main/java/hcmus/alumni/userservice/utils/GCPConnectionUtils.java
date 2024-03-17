0cf82cbcaddbba32a1d5bc5a46c96f507151694d
quinxii <82009267+quinxii@users.noreply.github.com>
2024-03-17 17:01:04 +0700

    Create GCPConnectionUtils.java
    update

---
+++ user-service/src/main/java/hcmus/alumni/userservice/utils/GCPConnectionUtils.java
@@ -0,0 +1,42 @@
+package hcmus.alumni.userservice.utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
+@Service
+public class GCPConnectionUtils {
+
+	@Value("${gcp.serviceAccountKeyPath}")
+	private String serviceAccountKeyPath;
+	@Value("${gcp.bucketName}")
+	private String bucketName;
+	@Value("${gcp.domainName}")
+	private String domainName;
+	private Storage storage = null;
+
+
+	public Storage getStorage() throws FileNotFoundException, IOException {
+		if (this.storage != null) {
+			return this.storage;
+		}
+		GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(serviceAccountKeyPath));
+		this.storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+		return this.storage;
+	}
+
+	public String getBucketName() {
+		return this.bucketName;
+	}
+
+	public String getDomainName() {
+		return domainName;
+	}
+}
