5e2d1a319638a42a10629336a4ced573a8818cd4
quinxii <82009267+quinxii@users.noreply.github.com>
2024-03-17 16:51:40 +0700

    Update UserServiceController.java
    update userservicecontroller

--- user-service/src/main/java/hcmus/alumni/userservice/controller/UserServiceController.java
+++ user-service/src/main/java/hcmus/alumni/userservice/controller/UserServiceController.java
@@ -1,92 +1,161 @@
 package hcmus.alumni.userservice.controller;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import hcmus.alumni.userservice.common.Privacy;
+import hcmus.alumni.userservice.common.UserUtils;
 import hcmus.alumni.userservice.model.UserModel;
+import hcmus.alumni.userservice.model.VerifyAlumniModel;
+import hcmus.alumni.userservice.repository.EmailActivationCodeRepository;
 import hcmus.alumni.userservice.repository.UserRepository;
+import hcmus.alumni.userservice.repository.VerifyAlumniRepository;
 import hcmus.alumni.userservice.utils.EmailSenderUtils;
+import hcmus.alumni.userservice.utils.ImageUtils;
 import hcmus.alumni.userservice.utils.PasswordUtils;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") // Allow requests from Web
 @RequestMapping("/user")
 public class UserServiceController {
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private VerifyAlumniRepository verifyAlumniRepository;
+	
+	@Autowired
+    private EmailActivationCodeRepository emailActivationCodeRepository;
+	
+	@Autowired
+	private ImageUtils imageUtils;
+	
+	private UserUtils userUtils = UserUtils.getInstance();
+	private PasswordUtils passwordUtils = PasswordUtils.getInstance();
+	private EmailSenderUtils emailSenderUtils = EmailSenderUtils.getInstance();
 
-    @PostMapping("/login")
-    public UserModel login(@RequestParam String email, @RequestParam String pass) {
-        UserModel user = new UserModel();
-        
-        // Find user by email
-        UserModel foundUser = userRepository.findByEmailAndPass(email,PasswordUtils.hashPassword(pass));
-        if (foundUser != null) {
-        	user = foundUser;
-            user.setLastLogin(new Date());
-            userRepository.save(user);
-        }
-        return user;
-    }
-    
-    @PostMapping("/sendAuthorizeCode")
-    public Boolean sendAuthorizeCode(@RequestParam String id) {
-    	String email = getUserEmail(id);
+	@PostMapping("/login")
+	public UserModel login(@RequestParam String email, @RequestParam String pass) {
+		UserModel user = new UserModel();
 
-        if (email == null) {
-            return false; 
-        }
+		// Find user by email
+		UserModel foundUser = userRepository.findByEmailAndPass(email, passwordUtils.hashPassword(pass));
+		if (foundUser != null) {
+			user = foundUser;
+			user.setLastLogin(new Date());
+			userRepository.save(user);
+		}
+		return user;
+	}
 
-        try {
-        	EmailSenderUtils.sendEmail(email);
-            return true; 
-        } catch (MailException e) {
-            e.printStackTrace();
-            return false; 
-        }
-    }
-    
-    private String getUserEmail(String userId) {
-        UserModel user = null;
-        UserModel foundUser = userRepository.findUserById(userId);
-        
-        if (foundUser != null) {
-            user = foundUser;
-        }
-        
-        return user != null ? user.getEmail() : null;
-    }
+	@PostMapping("/send-authorize-code")
+	public String sendAuthorizeCode(@RequestParam String email) {
+	    if (email == null || email.isEmpty()) {
+	        return "Send Authorize code failed: Email is required";
+	    }
 
-	/*
-	 * @PostMapping("/verifyAuthorizeCode") public Boolean
-	 * verifyAuthorizeCode(@RequestParam String authorizeCode) { UserModel user =
-	 * new UserModel();
-	 * 
-	 * // Find user by email UserModel foundUser =
-	 * userRepository.findByEmailAndPass(email,PasswordUtils.hashPassword(pass)); if
-	 * (foundUser != null) { user = foundUser; user.setLastLogin(new Date());
-	 * userRepository.save(user); } return false; }
-	 * 
-	 * @PostMapping("/signup") public UserModel signup(@RequestParam String
-	 * email, @RequestParam String pass, @RequestParam String fullName,
-	 * 
-	 * @RequestParam String studentId, @RequestParam String beginningYear) { //
-	 * Check if the user already exists if
-	 * (userRepository.findByEmail(newUser.getEmail()) != null) { // User already
-	 * exists return null; }
-	 * 
-	 * // Hash the password before saving it String hashedPassword =
-	 * PasswordUtils.hashPassword(newUser.getPass());
-	 * newUser.setPass(hashedPassword);
-	 * 
-	 * // Set the creation date newUser.setCreateAt(new Date());
-	 * 
-	 * // Save the new user return userRepository.save(newUser); }
-	 */
+	    try {
+	        emailSenderUtils.sendEmail(emailActivationCodeRepository, email);
+	        return "Send Authorize code success";
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return "Send Authorize code failed: " + e.getMessage();
+	    }
+	}
+
+	@PostMapping("/verify-authorize-code")
+	public String verifyAuthorizeCode(@RequestParam String email, @RequestParam String activationCode) {
+	    if (email == null || email.isEmpty() || activationCode == null || activationCode.isEmpty()) {
+	        return "Verify Authorize code failed: Email and activation code are required";
+	    }
+
+	    try {
+	        boolean isValid = userUtils.checkActivationCode(emailActivationCodeRepository, email, activationCode);
+	        if (isValid) {
+	            return "Verify Authorize code success: Activation code is valid";
+	        } else {
+	            return "Verify Authorize code failed: Activation code is invalid or expired";
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return "Verify Authorize code failed: " + e.getMessage();
+	    }
+	}
+
+	@PostMapping("/signup")
+	public String signup(@RequestParam String email,
+	                     @RequestParam String pass) {
+
+	    UserModel existingUser = userRepository.findByEmail(email);
+	    if (existingUser != null) {
+	        return "User with this email already exists";
+	    }
+
+	    UserModel newUser = new UserModel();
+	    
+	    // Generate UUID
+	    UUID uuid = UUID.randomUUID();
+	    String uuidString = uuid.toString(); 
+	    
+	    if(uuidString.length() != 36) {
+	        return "Error occurred during user signup";
+	    }
+	    newUser.setId(uuidString);
+	    newUser.setEmail(email);
+
+	    String hashedPassword = passwordUtils.hashPassword(pass);
+	    newUser.setPass(hashedPassword);
+	    
+	    newUser.setRoleId("8ea1665e-74b4-43ac-a966-bf10e938da42");
+	    newUser.setSexId("8ea1665e-74b4-43ac-a966-bf10e938da43");
+	    newUser.setStatusId("8ea1665e-74b4-43ac-a966-bf10e938da45");
+	    newUser.setEmailPrivacy(Privacy.PUBLIC);
+	    newUser.setPhonePrivacy(Privacy.PUBLIC);
+	    newUser.setSexPrivacy(Privacy.PUBLIC);
+	    newUser.setDobPrivacy(Privacy.PUBLIC);
+
+	    userRepository.save(newUser);
+
+	    return "Signup successful";
+	}
+
+	@PostMapping("/alumni-verification")
+	public ResponseEntity<String> createAlumniVerification(
+			@RequestParam(value = "avatar", required = false) MultipartFile avatar,
+			@RequestParam("full_name") String full_name,
+			@RequestParam(value = "student_id", required = false) String student_id,
+			@RequestParam(value = "beginning_year", required = false) Integer beginning_year,
+			@RequestParam(value = "social_media_link", required = false) String social_media_link) {
+		String userID = "8ea1665e-74b4-43ac-a966-bf10e938da44"; // delete after implementing jwt
+		VerifyAlumniModel verifyAlumni = new VerifyAlumniModel(userID, student_id, beginning_year, social_media_link);
+
+		try {
+			userRepository.setFullName(userID, full_name);
+			if (student_id != null || beginning_year != null || social_media_link != null) {
+				verifyAlumniRepository.save(verifyAlumni);
+			}
+			String avatarUrl = imageUtils.saveImageToStorage(imageUtils.getAvatarPath(), avatar, userID);
+			userRepository.setAvatarUrl(userID, avatarUrl);
+		} catch (IllegalArgumentException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return ResponseEntity.status(HttpStatus.CREATED).body("Post alumni verification successfully");
+	}
 }
