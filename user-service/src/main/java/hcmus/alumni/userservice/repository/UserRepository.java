e7e93fd479689ce6b86b548f457e3fca940be8eb
quinxii <82009267+quinxii@users.noreply.github.com>
2024-03-17 17:00:52 +0700

    Update UserRepository.java
    update

--- user-service/src/main/java/hcmus/alumni/userservice/repository/UserRepository.java
+++ user-service/src/main/java/hcmus/alumni/userservice/repository/UserRepository.java
@@ -1,18 +1,32 @@
 package hcmus.alumni.userservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import hcmus.alumni.userservice.model.UserModel;
 
 public interface UserRepository extends JpaRepository<UserModel, String> {
 
     UserModel findByEmailAndPass(String email, String pass);
+    UserModel findByEmail(String email);
     
     @Query("SELECT u FROM UserModel u WHERE u.id = :userId")
     UserModel findUserById(@Param("userId") String userId);
 
+    @Transactional
+    @Modifying
+    @Query("UPDATE UserModel u SET u.avatarUrl = :avatarUrl WHERE u.id = :userId")
+    int setAvatarUrl(@Param("userId") String userId, @Param("avatarUrl") String avatarUrl);
+    
+    @Transactional
+    @Modifying
+    @Query("UPDATE UserModel u SET u.fullName = :fullName WHERE u.id = :userId")
+    int setFullName(@Param("userId") String userId, @Param("fullName") String fullName);
+
+    
 }
 
 
