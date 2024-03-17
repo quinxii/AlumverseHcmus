c23d5073c41fa2de627f302c3e53715e8b348e0a
quinxii <82009267+quinxii@users.noreply.github.com>
2024-03-17 17:00:55 +0700

    Create VerifyAlumniRepository.java
    update

---
+++ user-service/src/main/java/hcmus/alumni/userservice/repository/VerifyAlumniRepository.java
@@ -0,0 +1,14 @@
+package hcmus.alumni.userservice.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import hcmus.alumni.userservice.model.VerifyAlumniModel;
+
+public interface VerifyAlumniRepository extends JpaRepository<VerifyAlumniModel, String> {
+
+	List<VerifyAlumniModel> findAllByIsDeleteEquals(Boolean isDelete);
+	Optional<VerifyAlumniModel> findByUserIdAndIsDeleteEquals(String userId, Boolean isDelete);
+}
