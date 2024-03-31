182b520f686e5dc1c7e718945ce0f4ad0ba7cbd6
HuynhNguyen0811 <81243529+HuynhNguyen0811@users.noreply.github.com>
2024-03-31 22:00:40 +0700

    Revert "create events service"
    This reverts commit b98aa922ddf365022802fbccb52887a28a115241.

--- events-service/src/main/java/hcmus/alumni/events/repository/EventsRepository.java
+++
@@ -1,7 +0,0 @@
-package hcmus.alumni.news.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewsRepository  extends JpaRepository<NewsRepository, String> {
-	long countByIsDeleteEquals(Boolean isDelete);
-}
