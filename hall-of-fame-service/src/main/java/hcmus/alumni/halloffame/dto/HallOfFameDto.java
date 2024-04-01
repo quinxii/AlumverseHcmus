7880f38deec62b44bb0e14a7a38ce7ebacfbca73
quinxii <82009267+quinxii@users.noreply.github.com>
2024-04-01 23:15:07 +0700

    Create HallOfFameDto.java
    hall of fame service

---
+++ hall-of-fame-service/src/main/java/hcmus/alumni/halloffame/dto/HallOfFameDto.java
@@ -0,0 +1,16 @@
+package hcmus.alumni.halloffame.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class HallOfFameDto {
+	private String id;
+	private String title;
+	private String content;
+	private String thumbnail;
+	private Integer views;
+}
