a2e2d9e1319033c22d896897983f4aba5a8f88dc
quinxii <82009267+quinxii@users.noreply.github.com>
2024-04-01 23:15:17 +0700

    Update HallOfFameRepository.java
    hall of fame service

--- hall-of-fame-service/src/main/java/hcmus/alumni/halloffame/repository/HallOfFameRepository.java
+++ hall-of-fame-service/src/main/java/hcmus/alumni/halloffame/repository/HallOfFameRepository.java
@@ -1,7 +1,16 @@
 package hcmus.alumni.halloffame.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface HallOfFameRepository extends JpaRepository<HallOfFameRepository, String> {
-	long countByIsDeleteEquals(Boolean isDelete);
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import hcmus.alumni.halloffame.model.HallOfFameModel;
+
+
+public interface HallOfFameRepository extends JpaRepository<HallOfFameModel, String> {
+	Optional<HallOfFameModel> findById(String id);
+	@Query("SELECT COUNT(n) FROM HallOfFameModel n JOIN n.status s WHERE s.name = :statusName")
+	Long getCountByStatus(@Param("statusName") String statusName);
 }
