26c9f72e879e25be753d09ff42b07620d8aa59d3
quinxii <82009267+quinxii@users.noreply.github.com>
2024-04-07 20:55:54 +0700

    Update HallOfFameRepository.java
    update

--- hall-of-fame-service/src/main/java/hcmus/alumni/halloffame/repository/HallOfFameRepository.java
+++ hall-of-fame-service/src/main/java/hcmus/alumni/halloffame/repository/HallOfFameRepository.java
@@ -1,16 +1,24 @@
 package hcmus.alumni.halloffame.repository;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import hcmus.alumni.halloffame.dto.IHallOfFameDto;
 import hcmus.alumni.halloffame.model.HallOfFameModel;
 
 
 public interface HallOfFameRepository extends JpaRepository<HallOfFameModel, String> {
+	Optional<IHallOfFameDto> findHallOfFameById(String id);
 	Optional<HallOfFameModel> findById(String id);
+	
 	@Query("SELECT COUNT(n) FROM HallOfFameModel n JOIN n.status s WHERE s.name = :statusName")
 	Long getCountByStatus(@Param("statusName") String statusName);
+	
+	@Query("SELECT n from NewsModel n JOIN n.status s WHERE s.name = \"Chờ\" AND n.publishedAt <= :now")
+	List<HallOfFameModel> getScheduledNews(Date now);
 }
