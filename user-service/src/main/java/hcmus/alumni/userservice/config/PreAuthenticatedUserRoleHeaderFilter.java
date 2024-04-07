39ebbfa547c950a4cb0cd143b4b66409f6da3aea
quinxii <82009267+quinxii@users.noreply.github.com>
2024-04-07 20:56:40 +0700

    Create PreAuthenticatedUserRoleHeaderFilter.java
    update

---
+++ user-service/src/main/java/hcmus/alumni/userservice/config/PreAuthenticatedUserRoleHeaderFilter.java
@@ -0,0 +1,49 @@
+package hcmus.alumni.userservice.config;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.List;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class PreAuthenticatedUserRoleHeaderFilter extends OncePerRequestFilter {
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Enumeration<String> roles = request.getHeaders("roles");
+		String userId = request.getHeader("userId");
+
+		// Create authentication object with extracted roles
+		Authentication authentication = new UsernamePasswordAuthenticationToken(userId, null, getAuthorities(roles));
+
+		// Set the authentication object to SecurityContextHolder
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		// Proceed with the filter chain
+		filterChain.doFilter(request, response);
+	}
+
+	public Collection<? extends GrantedAuthority> getAuthorities(Enumeration<String> roles) {
+		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+		while (roles.hasMoreElements()) {
+			authorities.add(new SimpleGrantedAuthority(roles.nextElement()));
+		}
+
+		return authorities;
+	}
+}
