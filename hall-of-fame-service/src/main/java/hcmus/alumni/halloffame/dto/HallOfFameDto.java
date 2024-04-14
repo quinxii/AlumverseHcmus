89863f38341d91c94ad46f70cbf1f19ac8e8212d
quinxii <82009267+quinxii@users.noreply.github.com>
2024-04-14 20:14:55 +0700

    Update HallOfFameDto.java
    update

--- hall-of-fame-service/src/main/java/hcmus/alumni/halloffame/dto/HallOfFameDto.java
+++ hall-of-fame-service/src/main/java/hcmus/alumni/halloffame/dto/HallOfFameDto.java
@@ -1,5 +1,9 @@
 package hcmus.alumni.halloffame.dto;
 
+import java.util.Date;
+import java.util.Set;
+
+import hcmus.alumni.halloffame.model.TagModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,11 +12,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class HallOfFameDto {
+
 	private String id;
 	private String title;
-	private String content;
+//	private String content;
 	private String thumbnail;
 	private Integer views;
 	private String faculty;
 	private Integer beginningYear;
+	
+	private Date publishedAt;
+	
+	private Set<TagModel> tags;
+
 }
