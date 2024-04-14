230f684611e178811eb04ae5a66c01eb1ffa5148
quinxii <82009267+quinxii@users.noreply.github.com>
2024-04-14 16:36:08 +0700

    Update AuthenticationGatewayFilterFactory.java
    change service name

--- api-gateway/src/main/java/hcmus/alumni/apigateway/AuthenticationGatewayFilterFactory.java
+++ api-gateway/src/main/java/hcmus/alumni/apigateway/AuthenticationGatewayFilterFactory.java
@@ -1,10 +1,9 @@
 package hcmus.alumni.apigateway;
 
-import io.jsonwebtoken.Claims;
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,12 +11,15 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
+
 import hcmus.alumni.apigateway.filter.RouteValidator;
 import hcmus.alumni.apigateway.utils.JwtUtils;
+import io.jsonwebtoken.Claims;
+import reactor.core.publisher.Mono;
 
 @Component
-public class AuthenticationGatewayFilterFactory extends AbstractGatewayFilterFactory<AuthenticationGatewayFilterFactory.Config> {
+public class AuthenticationGatewayFilterFactory
+		extends AbstractGatewayFilterFactory<AuthenticationGatewayFilterFactory.Config> {
 
 	@Autowired
 	private RouteValidator routeValidator;
@@ -29,31 +31,32 @@ public class AuthenticationGatewayFilterFactory extends AbstractGatewayFilterFac
 		super(Config.class);
 	}
 
-	public static class Config {}
+	public static class Config {
+	}
 
 	@Override
 	public GatewayFilter apply(Config config) {
 		return ((exchange, chain) -> {
 			ServerHttpRequest req = exchange.getRequest();
-            if (routeValidator.isSecured.test(req)) {
-                //header contains token or not
-                if (!req.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-                	return this.onError(exchange, HttpStatus.UNAUTHORIZED);
-                }
+			if (routeValidator.isSecured.test(req)) {
+				// header contains token or not
+				if (!req.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+					return this.onError(exchange, HttpStatus.UNAUTHORIZED);
+				}
 
-                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
-                if (authHeader != null && authHeader.startsWith("Bearer ")) {
-                    authHeader = authHeader.substring(7);
-                }
-                try {
-                    jwtUtils.validateToken(authHeader);
-                    req = updateRequest(exchange, authHeader);
-                } catch (Exception e) {
-                    return this.onError(exchange, HttpStatus.FORBIDDEN);
-                }
-                
-            }
-            return chain.filter(exchange.mutate().request(req).build());
+				String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+				if (authHeader != null && authHeader.startsWith("Bearer ")) {
+					authHeader = authHeader.substring(7);
+				}
+				try {
+					jwtUtils.validateToken(authHeader);
+					req = updateRequest(exchange, authHeader);
+				} catch (Exception e) {
+					return this.onError(exchange, HttpStatus.FORBIDDEN);
+				}
+
+			}
+			return chain.filter(exchange.mutate().request(req).build());
 		});
 	}
 
@@ -65,6 +68,12 @@ public class AuthenticationGatewayFilterFactory extends AbstractGatewayFilterFac
 
 	private ServerHttpRequest updateRequest(ServerWebExchange exchange, String token) {
 		Claims claims = jwtUtils.getAllClaimsFromToken(token);
-		return exchange.getRequest().mutate().header("userId", String.valueOf(claims.get("sub"))).build();
+		@SuppressWarnings("unchecked")
+		ArrayList<String> roles = (ArrayList<String>) claims.get("roles");
+		ServerHttpRequest modifiedRequest = exchange.getRequest().mutate().headers(httpHeaders -> {
+			httpHeaders.add("userId", String.valueOf(claims.get("sub")));
+			httpHeaders.addAll("roles", roles);
+		}).build();
+		return modifiedRequest;
 	}
 }
