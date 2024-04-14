b7a7f689c807e6b02aceac5803b5095614c60396
quinxii <82009267+quinxii@users.noreply.github.com>
2024-04-14 19:01:07 +0700

    Update HallOfFameRepository.java
    hof update

--- hall-of-fame-service/src/main/java/hcmus/alumni/halloffame/repository/HallOfFameRepository.java
+++ hall-of-fame-service/src/main/java/hcmus/alumni/halloffame/repository/HallOfFameRepository.java
@@ -8,17 +8,40 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
 import hcmus.alumni.halloffame.dto.IHallOfFameDto;
 import hcmus.alumni.halloffame.model.HallOfFameModel;
 
 
 public interface HallOfFameRepository extends JpaRepository<HallOfFameModel, String> {
 	Optional<IHallOfFameDto> findHallOfFameById(String id);
+	
 	Optional<HallOfFameModel> findById(String id);
+	
+	@Query("SELECT n FROM HallOfFameModel n JOIN n.status s WHERE s.id != 4 AND n.title like %:title%")
+	Page<IHallOfFameDto> searchHof(String title, Pageable pageable);
+	
+	@Query("SELECT n FROM HallOfFameModel n JOIN n.status s WHERE s.id = :statusId AND n.title like %:title%")
+	Page<IHallOfFameDto> searchHofByStatus(String title, Integer statusId, Pageable pageable);
 	
 	@Query("SELECT COUNT(n) FROM HallOfFameModel n JOIN n.status s WHERE s.name = :statusName")
 	Long getCountByStatus(@Param("statusName") String statusName);
 	
+	@Query("SELECT COUNT(n) FROM HallOfFameModel n JOIN n.status s WHERE s.id != 4")
+	Long getCountByNotDelete();
+
+	@Transactional
+	@Modifying
+	@Query("UPDATE NewsModel n SET n.views = n.views + 1 WHERE n.id = :id")
+	int viewsIncrement(String id);
+	
 	@Query("SELECT n from HallOfFameModel n JOIN n.status s WHERE s.name = \"Chờ\" AND n.publishedAt <= :now")
-	List<HallOfFameModel> getScheduledNews(Date now);
+	List<HallOfFameModel> getScheduledHof(Date now);
 }
