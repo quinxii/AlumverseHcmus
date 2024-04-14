af66147c4006f3fbe2f8ada788fd1b87afb0048c
quinxii <82009267+quinxii@users.noreply.github.com>
2024-04-14 20:14:59 +0700

    Update IHallOfFameDto.java
    update

--- hall-of-fame-service/src/main/java/hcmus/alumni/halloffame/dto/IHallOfFameDto.java
+++ hall-of-fame-service/src/main/java/hcmus/alumni/halloffame/dto/IHallOfFameDto.java
@@ -1,17 +1,45 @@
 package hcmus.alumni.halloffame.dto;
 
-public interface IHallOfFameDto {
+import java.util.Date;
+import java.util.Set;
 
-	interface StatusPost{
-		String getName();
-	}
-	
-	String getId();
-	String getTitle();
-	String getContent();
-	String getThumbnail();
-	Integer getViews();
-	StatusPost getStatus();
-	String getFaculty();
-	Integer getBeginningYear();
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public interface IHallOfFameDto {
+    interface User {
+        String getId(); // Added
+        String getFullName();
+    }
+
+    interface StatusPost {
+        Integer getId(); // Added
+        String getName();
+    }
+
+    interface Tag {
+        Integer getId();
+        String getName();
+    }
+
+    interface Faculty {
+        Integer getId();
+        String getName();
+    }
+
+    String getId();
+    String getTitle();
+    String getSummary();
+    String getContent();
+    String getThumbnail();
+    Integer getViews();
+    Date getCreateAt(); // Added
+    Date getUpdateAt();
+    Date getPublishedAt();
+    User getCreator();
+    Set<Tag> getTags();
+    Faculty getFaculty();
+    StatusPost getStatus();
+    Integer getBeginningYear();
 }
