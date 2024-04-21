0585af7f1085fdc1c415026b2c57ad74a16594a2
quinxii <82009267+quinxii@users.noreply.github.com>
2024-04-21 17:41:00 +0700

    Create FacultyModel.java
    update

---
+++ search-service/src/main/java/hcmus/alumni/search/model/FacultyModel.java
@@ -0,0 +1,48 @@
+package hcmus.alumni.search.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "[faculty]")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class FacultyModel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", nullable = false, columnDefinition = "TINYINT")
+	private Integer id;
+
+	@Column(name = "name", length = 100, nullable = false, unique = true)
+	private String name;
+
+	@Column(name = "description", columnDefinition = "TINYTEXT")
+	private String description;
+
+	@CreationTimestamp
+	@Column(name = "create_at")
+	private Date createAt;
+
+	@Column(name = "update_at")
+	private Date updateAt;
+
+	@Column(name = "is_delete", columnDefinition = "TINYINT(1) DEFAULT(0)")
+	private Boolean isDelete;
+
+	public FacultyModel(Integer id) {
+		this.id = id;
+	}
+}
