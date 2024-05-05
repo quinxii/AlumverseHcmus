38879477fa6b474760c60fe84df138788c7d5805
quinxii <82009267+quinxii@users.noreply.github.com>
2024-05-05 12:59:40 +0700

    Update UserModel.java
    commit

--- search-service/src/main/java/hcmus/alumni/search/model/UserModel.java
+++ search-service/src/main/java/hcmus/alumni/search/model/UserModel.java
@@ -2,16 +2,21 @@ package hcmus.alumni.search.model;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.UUID;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import hcmus.alumni.search.common.Privacy;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -24,94 +29,96 @@ import lombok.Data;
 public class UserModel implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	public enum Privacy {
-		PUBLIC, FRIEND, ONLYME
-	}
-
 	@Id
-	@Column(name = "id", length = 36, nullable = false)
-	private String id;
+    @Column(name = "id", length = 36, nullable = false)
+    private String id;
 
-	@Column(name = "email", length = 255, nullable = false, unique = true)
-	private String email;
+    @Column(name = "email", length = 255, nullable = false, unique = true)
+    private String email;
 
-	@Column(name = "pass", length = 60, nullable = false)
-	private String pass;
+    @Column(name = "pass", length = 60, nullable = false)
+    private String pass;
+    
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+            )
+    private Set<RoleModel> roles = new HashSet<>();
 
-	@Column(name = "full_name", length = 100)
-	private String fullName;
+    @Column(name = "full_name", length = 100)
+    private String fullName;
 
-	@Column(name = "phone", length = 15)
-	private String phone;
+    @Column(name = "phone", length = 15)
+    private String phone;
 
-	@Column(name = "sex_id")
-	private Integer sex;
+	@OneToOne
+    @JoinColumn(name = "sex_id") // Foreign key constraint
+	private SexModel sex;
 
-	@Column(name = "dob")
-	private Date dob;
+    @Column(name = "dob")
+    private Date dob;
+    
+    @Column(name = "social_media_link", columnDefinition = "TINYTEXT")
+    private String socialMediaLink;
+    
+	@OneToOne
+    @JoinColumn(name = "faculty_id") // Foreign key constraint
+	private FacultyModel faculty;
+    
+    @Column(name = "degree", length = 50)
+    private String degree;
+    
+    @Column(name = "about_me", columnDefinition = "TEXT")
+    private String aboutMe;
 
-	@Column(name = "social_media_link", columnDefinition = "TINYTEXT")
-	private String socialMediaLink;
+    @Column(name = "avatar_url", columnDefinition = "TINYTEXT")
+    private String avatarUrl;
 
-    @Column(name = "faculty_id")
-	private Integer facultyId;
+    @Column(name = "cover_url", columnDefinition = "TINYTEXT")
+    private String coverUrl;
 
-	@Column(name = "degree", length = 50)
-	private String degree;
-
-	@Column(name = "about_me", columnDefinition = "TEXT")
-	private String aboutMe;
-
-	@Column(name = "avatar_url", columnDefinition = "TINYTEXT")
-	private String avatarUrl;
-
-	@Column(name = "cover_url", columnDefinition = "TINYTEXT")
-	private String coverUrl;
-
-	@Column(name = "status_id")
-	private Integer statusId;
+    @Column(name = "status_id")
+    private Integer statusId;
 
 	@CreationTimestamp
-	@Column(name = "create_at")
-	private Date createAt;
+    @Column(name = "create_at")
+    private Date createAt;
 
-	@Column(name = "update_at")
-	private Date updateAt;
+    @Column(name = "update_at")
+    private Date updateAt;
 
-	@Column(name = "last_login")
-	private Date lastLogin;
+    @Column(name = "last_login")
+    private Date lastLogin;
 
-	@Column(name = "online_status")
-	private boolean onlineStatus;
+    @Column(name = "online_status")
+    private boolean onlineStatus;
 
-	@Column(name = "email_privacy", columnDefinition = "ENUM('PUBLIC', 'FRIEND', 'ONLYME') DEFAULT('PUBLIC')")
-	@Enumerated(EnumType.STRING)
-	private Privacy emailPrivacy = Privacy.PUBLIC;
+    @Column(name = "email_privacy", columnDefinition = "ENUM('PUBLIC', 'FRIEND', 'ONLYME') DEFAULT('PUBLIC')")
+    @Enumerated(EnumType.STRING)
+    private Privacy emailPrivacy = Privacy.PUBLIC;
 
-	@Column(name = "phone_privacy", columnDefinition = "ENUM('PUBLIC', 'FRIEND', 'ONLYME') DEFAULT('PUBLIC')")
-	@Enumerated(EnumType.STRING)
-	private Privacy phonePrivacy = Privacy.PUBLIC;
+    @Column(name = "phone_privacy", columnDefinition = "ENUM('PUBLIC', 'FRIEND', 'ONLYME') DEFAULT('PUBLIC')")
+    @Enumerated(EnumType.STRING)
+    private Privacy phonePrivacy = Privacy.PUBLIC;
 
-	@Column(name = "sex_privacy", columnDefinition = "ENUM('PUBLIC', 'FRIEND', 'ONLYME') DEFAULT('PUBLIC')")
-	@Enumerated(EnumType.STRING)
-	private Privacy sexPrivacy = Privacy.PUBLIC;
+    @Column(name = "sex_privacy", columnDefinition = "ENUM('PUBLIC', 'FRIEND', 'ONLYME') DEFAULT('PUBLIC')")
+    @Enumerated(EnumType.STRING)
+    private Privacy sexPrivacy = Privacy.PUBLIC;
 
-	@Column(name = "dob_privacy", columnDefinition = "ENUM('PUBLIC', 'FRIEND', 'ONLYME') DEFAULT('PUBLIC')")
-	@Enumerated(EnumType.STRING)
-	private Privacy dobPrivacy = Privacy.PUBLIC;
-
-	@Column(name = "faculty_privacy", columnDefinition = "ENUM('PUBLIC', 'FRIEND', 'ONLYME') DEFAULT('PUBLIC')")
-	@Enumerated(EnumType.STRING)
-	private Privacy facultyPrivacy = Privacy.PUBLIC;
-
-	public UserModel() {}
-	
-	public UserModel(String id) {
-		this.id = id;
-	}
-
-	public UserModel(String email, String pass) {
-		this.email = email;
-		this.pass = pass;
+    @Column(name = "dob_privacy", columnDefinition = "ENUM('PUBLIC', 'FRIEND', 'ONLYME') DEFAULT('PUBLIC')")
+    @Enumerated(EnumType.STRING)
+    private Privacy dobPrivacy = Privacy.PUBLIC;
+    
+    @Column(name = "faculty_privacy", columnDefinition = "ENUM('PUBLIC', 'FRIEND', 'ONLYME') DEFAULT('PUBLIC')")
+    @Enumerated(EnumType.STRING)
+    private Privacy facultyPrivacy = Privacy.PUBLIC;
+    
+    public UserModel() {}
+    
+    public UserModel(String email, String pass) {
+    	this.email = email;
+    	this.pass = pass;
 	}
 }
