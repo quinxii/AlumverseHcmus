85e5b165010a137ec989fd2fca333229305314b3
quinxii <82009267+quinxii@users.noreply.github.com>
2024-05-05 12:59:44 +0700

    Create SexModel.java
    commit

---
+++ search-service/src/main/java/hcmus/alumni/search/model/SexModel.java
@@ -0,0 +1,35 @@
+package hcmus.alumni.search.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "[sex]")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SexModel implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", nullable = false, columnDefinition = "TINYINT")
+    private Integer id;
+	
+	@Column(name = "name", length = 50, nullable = false, unique = true)
+    private String name;
+	
+	@Column(name = "description", columnDefinition = "TINYTEXT")
+    private String description;
+	
+	@Column(name = "is_delete", columnDefinition = "TINYINT(1) DEFAULT(0)")
+    private Boolean isDelete;
+}
