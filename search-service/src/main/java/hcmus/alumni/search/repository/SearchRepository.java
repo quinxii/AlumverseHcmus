cd01334bd142fb6e46ab7ecc88759dc346ad0f6d
quinxii <82009267+quinxii@users.noreply.github.com>
2024-05-06 22:51:56 +0700

    Update SearchRepository.java
    update search with faculty and fullname

--- search-service/src/main/java/hcmus/alumni/search/repository/SearchRepository.java
+++ search-service/src/main/java/hcmus/alumni/search/repository/SearchRepository.java
@@ -11,27 +11,14 @@ import hcmus.alumni.search.model.UserModel;
 
 public interface SearchRepository extends JpaRepository<UserModel, String> {
 
-//	@Query("SELECT DISTINCT u " + 
-//		   "FROM UserModel u " + 
-//		   "LEFT JOIN u.status s " + 
-//		   "LEFT JOIN u.faculty f " + 
-//		   "WHERE (:statusId IS NULL OR s.id = :statusId) " + 
-//		   "AND (:facultyId IS NULL OR f.id = :facultyId) " + 
-//		   "AND (:beginningYear IS NULL OR u.beginningYear = :beginningYear) " + 
-//		   "AND s.id != 4 " +
-//		   "AND u.fullName LIKE %:fullName%")
-//	Page<ISearchDto> searchUsers(String fullName, Integer statusId,
-//			Integer facultyId, Integer beginningYehar, Pageable pageable);
-
 	@Query("SELECT DISTINCT u " + 
 		       "FROM UserModel u " + 
 		       "LEFT JOIN u.status s " + 
 		       "LEFT JOIN u.faculty f " + 
-		       "WHERE (:statusId IS NULL OR s.id = :statusId) " + 
-		       "AND (:facultyId IS NULL OR f.id = :facultyId) " + 
-		       "AND s.id != 4 " +
-		       "AND u.fullName LIKE %:fullName%")
-	Page<ISearchDto> searchUsers(String fullName, Integer statusId, Integer facultyId, Pageable pageable);
+		       "WHERE u.fullName LIKE %:fullName% " +
+		       "AND ((:facultyName IS NULL OR :facultyName = '') OR LOWER(f.name) LIKE %:facultyName%) " + 
+		       "AND ((:statusName IS NULL OR :statusName = '') OR LOWER(s.name) LIKE %:statusName%)")
+		Page<ISearchDto> searchUsers(String fullName, String statusName, String facultyName, Pageable pageable);
 
 	@Query("SELECT COUNT(u) FROM UserModel u JOIN u.status s WHERE s.name = :statusName")
 	Long getCountByStatus(@Param("statusName") String statusName);
