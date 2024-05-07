dfd5519063ba7cc76476f03a50473e1fe46bf2ef
quinxii <82009267+quinxii@users.noreply.github.com>
2024-05-08 00:12:26 +0700

    Create ISearchDto.java
    search user

---
+++ user-service/src/main/java/hcmus/alumni/userservice/dto/ISearchDto.java
@@ -0,0 +1,31 @@
+package hcmus.alumni.userservice.dto;
+
+public interface ISearchDto {
+    interface User {
+        String getId(); // Added
+        String getFullName();
+        Alumni getAlumni();
+    }
+
+    interface Faculty {
+        Integer getId();
+        String getName();
+    }
+    
+    interface StatusUser {
+        Integer getId(); // Added
+        String getName();
+    }
+    
+    interface Alumni {
+        Integer getBeginningYear(); 
+    }
+
+    String getId();
+    String getFullName();
+    String getAvatarUrl();
+    Faculty getFaculty();
+    String getSocialMediaLink();
+    Alumni getAlumni();
+
+}
