495b514e39339d332464c15f0ba586403a2fa43b
quinxii <82009267+quinxii@users.noreply.github.com>
2024-05-08 00:12:12 +0700

    Update UserRepository.java
    search user

--- user-service/src/main/java/hcmus/alumni/userservice/repository/UserRepository.java
+++ user-service/src/main/java/hcmus/alumni/userservice/repository/UserRepository.java
@@ -2,12 +2,15 @@ package hcmus.alumni.userservice.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import hcmus.alumni.userservice.dto.ISearchDto;
 import hcmus.alumni.userservice.model.FacultyModel;
 import hcmus.alumni.userservice.model.UserModel;
 
@@ -40,4 +43,19 @@ public interface UserRepository extends JpaRepository<UserModel, String> {
 
 	@Query("SELECT u.avatarUrl FROM UserModel u WHERE u.id = :userId")
 	String findAvatarUrlByUserId(@Param("userId") String userId);
+	
+	@Query("SELECT DISTINCT u " + 
+	       "FROM UserModel u " + 
+	       "LEFT JOIN u.status s " + 
+	       "LEFT JOIN u.faculty f " + 
+	       "LEFT JOIN u.alumni a " + 
+	       "WHERE u.fullName LIKE %:fullName% " +
+	       "AND ((:facultyName IS NULL OR :facultyName = '') OR LOWER(f.name) LIKE %:facultyName%) " + 
+	       "AND ((:statusName IS NULL OR :statusName = '') OR LOWER(s.name) LIKE %:statusName%) " +
+	       "AND ((:beginningYear IS NULL OR :beginningYear = 0) OR a.beginningYear = :beginningYear)")
+	Page<ISearchDto> searchUsers(String fullName, String statusName, String facultyName, Integer beginningYear, Pageable pageable);
+
+	@Query("SELECT COUNT(u) FROM UserModel u JOIN u.status s WHERE s.name = :statusName")
+	Long getCountByStatus(@Param("statusName") String statusName);
+
 }
