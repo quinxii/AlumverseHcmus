edd85217d60763db3bb0ffa6f9b414b4c5611638
quinxii <82009267+quinxii@users.noreply.github.com>
2024-05-21 22:27:20 +0700

    Update EmailSenderUtils.java
    create user and reset pwd

--- auth-service/src/main/java/hcmus/alumni/authservice/utils/EmailSenderUtils.java
+++ auth-service/src/main/java/hcmus/alumni/authservice/utils/EmailSenderUtils.java
@@ -57,4 +57,30 @@ public class EmailSenderUtils {
         	System.err.println("Error sending email: " + e.getMessage());
         }
     }
+    
+    public void sendPasswordResetEmail(String userEmail, String resetUrl) {
+        Courier.init("PK_PROD_D8HF91KG5AM7G4HBKFEECG9KARHM");
+
+        SendEnhancedRequestBody sendEnhancedRequestBody = new SendEnhancedRequestBody();
+        SendRequestMessage sendRequestMessage = new SendRequestMessage();
+        HashMap<String, String> to = new HashMap<String, String>();
+        to.put("email", userEmail);
+        sendRequestMessage.setTo(to);
+        sendRequestMessage.setTemplate("KGA5V18RZ8428BPEA18A0ABTRQRX"); 
+
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("recipientName", userEmail);
+        data.put("resetUrl", resetUrl);
+
+        sendRequestMessage.setData(data);
+
+        sendEnhancedRequestBody.setMessage(sendRequestMessage);
+
+        try {
+            SendEnhancedResponseBody response = new SendService().sendEnhancedMessage(sendEnhancedRequestBody);
+            System.out.println("Password reset email sent successfully to " + userEmail);
+        } catch (IOException e) {
+            System.err.println("Error sending password reset email: " + e.getMessage());
+        }
+    }
 }
