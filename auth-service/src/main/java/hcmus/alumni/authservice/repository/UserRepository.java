480eaace36d4035fcf49f246e9e6054e20a1f814
quinxii <82009267+quinxii@users.noreply.github.com>
2024-05-25 15:01:45 +0700

    Update UserRepository.java
    update user and set authenticate

--- auth-service/src/main/java/hcmus/alumni/authservice/repository/UserRepository.java
+++ auth-service/src/main/java/hcmus/alumni/authservice/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package hcmus.alumni.authservice.repository;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -24,5 +25,11 @@ public interface UserRepository extends JpaRepository<UserModel, String> {
     @Modifying
     @Query("UPDATE UserModel u SET u.lastLogin = :lastLogin WHERE u.email = :email")
     int setLastLogin(@Param("email") String email, @Param("lastLogin") Date lastLogin);
+    
+    @Query(value = "select distinct p.name from role_permission rp " +
+            "join role r on r.id = rp.role_id and r.is_delete = false " +
+            "join permission p on p.id = rp.permission_id and p.is_delete = false " +
+            "where r.name in :role and p.name like :domain% and rp.is_delete = false", nativeQuery = true)
+    List<String> getPermissions(List<String> role, String domain);
 }
 
