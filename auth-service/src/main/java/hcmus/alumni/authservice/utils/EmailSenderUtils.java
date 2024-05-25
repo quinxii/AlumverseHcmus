c52f94e8e6cef847549904378fa1f95060716a71
quinxii <82009267+quinxii@users.noreply.github.com>
2024-05-25 13:32:10 +0700

    Update EmailSenderUtils.java
    update create user and reset pwd and login

--- auth-service/src/main/java/hcmus/alumni/authservice/utils/EmailSenderUtils.java
+++ auth-service/src/main/java/hcmus/alumni/authservice/utils/EmailSenderUtils.java
@@ -33,7 +33,7 @@ public class EmailSenderUtils {
     public void sendEmail(EmailActivationCodeRepository emailActivationCodeRepository, String userEmail) throws Exception {
     	String authorizeCode = authorizationCodeGeneratorUtils.generateAuthorizeCode();
     	
-    	Courier.init("pk_test_Z0DMJFA8XYM8VFNMDE8RV0BYAKGS");
+    	Courier.init("pk_prod_D8HF91KG5AM7G4HBKFEECG9KARHM");
 
         SendEnhancedRequestBody sendEnhancedRequestBody = new SendEnhancedRequestBody();
         SendRequestMessage sendRequestMessage = new SendRequestMessage();
@@ -58,29 +58,55 @@ public class EmailSenderUtils {
         }
     }
     
-    public void sendPasswordResetEmail(String userEmail, String resetUrl) {
-        Courier.init("PK_PROD_D8HF91KG5AM7G4HBKFEECG9KARHM");
+    public void sendPasswordEmail(String userEmail, String newPwd) throws Exception {
+    	Courier.init("pk_prod_D8HF91KG5AM7G4HBKFEECG9KARHM");
 
         SendEnhancedRequestBody sendEnhancedRequestBody = new SendEnhancedRequestBody();
         SendRequestMessage sendRequestMessage = new SendRequestMessage();
         HashMap<String, String> to = new HashMap<String, String>();
         to.put("email", userEmail);
         sendRequestMessage.setTo(to);
-        sendRequestMessage.setTemplate("KGA5V18RZ8428BPEA18A0ABTRQRX"); 
+        sendRequestMessage.setTemplate("M9A9EPFXWK4V43JEK8HNHG72GCPW");
 
-        HashMap<String, Object> data = new HashMap<>();
+        HashMap<String, Object> data = new HashMap<String, Object>();
         data.put("recipientName", userEmail);
-        data.put("resetUrl", resetUrl);
+        data.put("newPwd", newPwd);
 
         sendRequestMessage.setData(data);
 
         sendEnhancedRequestBody.setMessage(sendRequestMessage);
 
         try {
-            SendEnhancedResponseBody response = new SendService().sendEnhancedMessage(sendEnhancedRequestBody);
-            System.out.println("Password reset email sent successfully to " + userEmail);
+          SendEnhancedResponseBody response = new SendService().sendEnhancedMessage(sendEnhancedRequestBody);
+          System.out.println("Password sent successfully to " + userEmail);
         } catch (IOException e) {
-            System.err.println("Error sending password reset email: " + e.getMessage());
+        	System.err.println("Error sending email: " + e.getMessage());
         }
     }
+    
+//    public void sendPasswordResetEmail(String userEmail, String resetUrl) {
+//        Courier.init("PK_PROD_D8HF91KG5AM7G4HBKFEECG9KARHM");
+//
+//        SendEnhancedRequestBody sendEnhancedRequestBody = new SendEnhancedRequestBody();
+//        SendRequestMessage sendRequestMessage = new SendRequestMessage();
+//        HashMap<String, String> to = new HashMap<String, String>();
+//        to.put("email", userEmail);
+//        sendRequestMessage.setTo(to);
+//        sendRequestMessage.setTemplate("KGA5V18RZ8428BPEA18A0ABTRQRX"); 
+//
+//        HashMap<String, Object> data = new HashMap<>();
+//        data.put("recipientName", userEmail);
+//        data.put("resetUrl", resetUrl);
+//
+//        sendRequestMessage.setData(data);
+//
+//        sendEnhancedRequestBody.setMessage(sendRequestMessage);
+//
+//        try {
+//            SendEnhancedResponseBody response = new SendService().sendEnhancedMessage(sendEnhancedRequestBody);
+//            System.out.println("Password reset email sent successfully to " + userEmail);
+//        } catch (IOException e) {
+//            System.err.println("Error sending password reset email: " + e.getMessage());
+//        }
+//    }
 }
