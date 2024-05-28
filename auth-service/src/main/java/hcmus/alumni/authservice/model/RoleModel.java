e7efcc021caadcf0478a803870d61e330c8634ae
quinxii <82009267+quinxii@users.noreply.github.com>
2024-05-29 00:14:30 +0700

    Update RoleModel.java
    fix and unit test after team review

--- auth-service/src/main/java/hcmus/alumni/authservice/model/RoleModel.java
+++ auth-service/src/main/java/hcmus/alumni/authservice/model/RoleModel.java
@@ -2,19 +2,30 @@ package hcmus.alumni.authservice.model;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import hcmus.alumni.authservice.dto.RoleRequestDto;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -26,7 +37,7 @@ public class RoleModel implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", nullable = false, columnDefinition = "TINYINT")
 	private Integer id;
 
@@ -47,7 +58,55 @@ public class RoleModel implements Serializable {
 	@Column(name = "is_delete", columnDefinition = "TINYINT(1) DEFAULT(0)")
 	private Boolean isDelete = false;
 
+	@Getter(value = AccessLevel.NONE)
+	@OrderBy("id ASC")
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "role_permission", joinColumns = @JoinColumn(name = "role_id"), inverseJoinColumns = @JoinColumn(name = "permission_id"))
+	private Set<PermissionModel> permissions = new HashSet<>();
+
 	public RoleModel(Integer id) {
 		this.id = id;
 	}
+
+	public RoleModel(RoleRequestDto role) {
+		this.name = role.getName();
+		this.description = role.getDescription();
+		role.getPermissions().forEach(permission -> {
+			this.permissions.add(new PermissionModel(permission.getId()));
+		});
+	}
+
+	public RoleModel(RoleRequestDto role, Integer id) {
+		this.id = id;
+		this.name = role.getName();
+		this.description = role.getDescription();
+		role.getPermissions().forEach(permission -> {
+			this.permissions.add(new PermissionModel(permission.getId()));
+		});
+	}
+
+	public void clearPermissions() {
+		this.permissions.clear();
+	}
+
+	public void addPermission(PermissionModel permission) {
+		this.permissions.add(permission);
+	}
+
+	public void addPermissions(List<PermissionModel> permissions) {
+		this.permissions.addAll(permissions);
+	}
+
+	public void removePermission(PermissionModel permission) {
+		this.permissions.remove(permission);
+	}
+	
+
+	public Set<String> getPermissions() {
+        Set<String> permissionNames = new HashSet<>();
+        for (PermissionModel permission : permissions) {
+            permissionNames.add(permission.getName());
+        }
+        return permissionNames;
+    }
 }
