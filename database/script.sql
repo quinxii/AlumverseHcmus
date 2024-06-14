706cfd17b104eba00e3d63451ce637204934319e
HuynhNguyen0811 <81243529+HuynhNguyen0811@users.noreply.github.com>
2024-06-14 21:17:33 +0700

    Add notification tables script.sql

--- database/script.sql
+++ database/script.sql
@@ -834,6 +834,63 @@ CREATE TABLE
             KEY `user_id` (`user_id`),
             CONSTRAINT `passwo_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+    
+-- Notification object
+DROP TABLE IF EXISTS notification_object;
+
+CREATE TABLE notification_object (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    entity_type INT UNSIGNED NOT NULL,
+    entity_id INT UNSIGNED NOT NULL,
+    created_on DATETIME NOT NULL,
+    status TINYINT NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+-- Notification
+DROP TABLE IF EXISTS notification;
+
+CREATE TABLE notification (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    notification_object_id INT UNSIGNED NOT NULL,
+    notifier_id INT UNSIGNED NOT NULL,
+    status TINYINT NOT NULL,
+    PRIMARY KEY (id),
+    INDEX fk_notification_object_idx (notification_object_id ASC),
+    INDEX fk_notification_notifier_id_idx (notifier_id ASC),
+    CONSTRAINT fk_notification_object
+        FOREIGN KEY (notification_object_id)
+        REFERENCES notification_object (id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT fk_notification_notifier_id
+        FOREIGN KEY (notifier_id)
+        REFERENCES user (id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+-- Notification change
+DROP TABLE IF EXISTS notification_change;
+
+CREATE TABLE notification_change (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    notification_object_id INT UNSIGNED NOT NULL,
+    actor_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    INDEX fk_notification_object_idx_2 (notification_object_id ASC),
+    INDEX fk_notification_actor_id_idx (actor_id ASC),
+    CONSTRAINT fk_notification_object_2
+        FOREIGN KEY (notification_object_id)
+        REFERENCES notification_object (id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT fk_notification_actor_id_idx
+        FOREIGN KEY (actor_id)
+        REFERENCES user (id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 
 -- INDEX
 ALTER TABLE `group` ADD INDEX idx_creator (creator);
