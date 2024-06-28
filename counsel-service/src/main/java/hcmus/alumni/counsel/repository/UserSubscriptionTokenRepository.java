dac3ea9eb3b35f7edd85bbbb39eb8bfaea5dbb6c
HuynhNguyen0811 <81243529+HuynhNguyen0811@users.noreply.github.com>
2024-06-28 17:53:15 +0700

    Solve duplicate token problem pt.2

--- counsel-service/src/main/java/hcmus/alumni/counsel/repository/UserSubscriptionTokenRepository.java
+++ counsel-service/src/main/java/hcmus/alumni/counsel/repository/UserSubscriptionTokenRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import hcmus.alumni.counsel.model.UserSubscriptionTokenId;
 import hcmus.alumni.counsel.model.UserSubscriptionTokenModel;
 
-public interface UserSubscriptionTokenRepository extends JpaRepository<UserSubscriptionTokenModel, String> {
-	@Query("SELECT ust.token FROM UserSubscriptionTokenModel ust WHERE ust.userId = :userId AND ust.isDelete = false")
-	List<String> findAllTokensByUserId(@Param("userId") String userId);
+public interface UserSubscriptionTokenRepository extends JpaRepository<UserSubscriptionTokenModel, UserSubscriptionTokenId> {
+	@Query("SELECT ust.id.token FROM UserSubscriptionTokenModel ust WHERE ust.id.userId = :userId AND ust.isDelete = false")
+    List<String> findAllTokensByUserId(@Param("userId") String userId);
 }
