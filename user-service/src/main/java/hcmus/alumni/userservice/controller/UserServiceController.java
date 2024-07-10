98015659d55a10f82774764bc12943ad8be1ee84
HuynhNguyen0811 <81243529+HuynhNguyen0811@users.noreply.github.com>
2024-07-10 12:38:05 +0700

    Minor Fix friend notifications

--- user-service/src/main/java/hcmus/alumni/userservice/controller/UserServiceController.java
+++ user-service/src/main/java/hcmus/alumni/userservice/controller/UserServiceController.java
@@ -1238,6 +1238,7 @@ public class UserServiceController {
 
 		friendRequestRepository.save(friendRequest);
 
+		notificationService.deleteNotification("request_friend", NotificationType.DELETE, user.getId());
 		notificationService.createNotification("request_friend", NotificationType.CREATE, 
 			friendId, user, friend, 
 			user.getFullName() + " đã gửi một lời mời kết bạn", null);
