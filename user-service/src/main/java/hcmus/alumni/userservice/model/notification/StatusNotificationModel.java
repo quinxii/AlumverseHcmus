cb53b54fc42d3dca9641a2acd1881f1df6bb093a
Samuel <20127610@student.hcmus.edu.vn>
2024-07-11 11:01:44 +0700

    fix library name

--- user-service/src/main/java/hcmus/alumni/userservice/model/notification/StatusNotificationModel.java
+++ user-service/src/main/java/hcmus/alumni/userservice/model/notification/StatusNotificationModel.java
@@ -4,12 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 import java.io.Serializable;
 
