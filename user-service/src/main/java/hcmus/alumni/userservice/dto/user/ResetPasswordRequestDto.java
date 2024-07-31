4a6eb7d4f75664453b54109e1132c6f5628bb439
Samuel <20127610@student.hcmus.edu.vn>
2024-07-31 11:30:45 +0700

    Create and adjust api endpoint
    Tạo api endpoint cho tự xóa tài khoản của bản thân
    Chỉnh sửa lại API đổi mật khẩu, đổi url endpoint thành change-password, bỏ email trong body

--- user-service/src/main/java/hcmus/alumni/userservice/dto/user/ResetPasswordRequestDto.java
+++ user-service/src/main/java/hcmus/alumni/userservice/dto/user/ResetPasswordRequestDto.java
@@ -12,7 +12,6 @@ import lombok.NoArgsConstructor;
 public class ResetPasswordRequestDto implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    String email;
     String oldPassword;
     String newPassword;
 }
